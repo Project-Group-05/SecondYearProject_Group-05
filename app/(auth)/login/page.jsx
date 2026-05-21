8a4f71927f232b730f6214e8aa45b1163503a576
sandaminim03 <sandaminim03@gmail.com>
2026-05-21 18:10:48 +0530

    Update page.jsx

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -3,16 +3,18 @@ import styles from './login.module.css';
 import Link from 'next/link';
 
 
+
 export default function LoginPage() {
-  return (
-  <div className={styles.container}>
-        <div className={styles.glassCard}>
-          <div className={styles.header}>
-            <h1 className={styles.appNameText}>Adaptive Chemistry</h1>
-          </div>
-  
-          <LoginForm />
+ return (
+    <div className={styles.container}>
+      <div className={styles.glassCard}>
+        <div className={styles.header}>
+          <h1 className={styles.appNameText}>Edu<span className={styles.logoAccent}>FX</span></h1>
         </div>
+
+        <LoginForm />
+
       </div>
+    </div>
   );
 }
